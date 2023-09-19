b926f3a2e31efaef7e6b178bf7173bf72891fb87
Christian Bockshorn <bockshorn@gmx.net>
2023-09-19 21:44:39 +0200

    js-pokemonLoad

--- script.js
+++ script.js
@@ -10,10 +10,11 @@ async function loadPokemon() {
     document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
     // renderPokemonInfo();
     loadPokemon2();
+    renderAllPokemon()
 }
 
 // function renderPokemonInfo() {
-    
+
 //     document.getElementById('pokemonImage').src = currentPokemon['sprites']['front_shiny'];
 // }
 
@@ -24,13 +25,36 @@ async function loadPokemon2() {
     let response = await fetch(url);
     let currentPokemon = await response.json();
 
-    console.log('loaded pokemon', currentPokemon);
+    console.log('loaded pokemon2', currentPokemon);
 
     for (let i = 0; i < pokemonName.length; i++) {
         const name = pokemonName[i];
 
         document.getElementById('pokemonName').innerHTML = currentPokemon['results'];
-        
+
     }
-    
+
+}
+
+
+async function renderAllPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/';
+    let response = await fetch(url);
+    let currentPokemon = await response.json();
+
+    for (let p = 0; p < allPokemon.length; p++) {
+        const element = allPokemon[p];
+
+        document.getElementById(`card${p}`).innerHTML += `
+            <div id="pokedex">
+                <h1 id="pokemonName">Name</h1>
+                <img id="pokemonImage">
+                <p class="stats">stats</p>
+            </div>
+        `;
+
+        
+
+    }
+
 }
