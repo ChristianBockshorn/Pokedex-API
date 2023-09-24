dcf04f1562e17f0d9d1378c5416000f989ebe68b
Christian Bockshorn <bockshorn@gmx.net>
2023-09-24 14:43:26 +0200

    add id at  pokedex

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let currentPokemon;
-let pokemon = [];
 let allPokemon = [];
 let loadStop = 20;
 let currentPokemonIndex = 0;
 
 
-async function loadPokemon3() {
+async function loadPokemon() {
     const startIndex = currentPokemonIndex + 1;
     const endIndex = currentPokemonIndex + loadStop;
 
@@ -26,17 +25,23 @@ function genPokeCard(i, currentPokemon) {
     let pokeCard = document.getElementById(`card`);
     pokeCard.innerHTML += `
     <div id="pokedex${i}" class="pokedex">
-        <h1 id="pokemonName${i}">Name</h1>
+        <div class="pokemonHeader">
+            <h1 id="pokemonName${i}" class="pokemonName">Name</h1>    
+            <h2 id="pokemonId${i}">#</h2>
+            
+        </div>
+        
         <img id="pokemonImage${i}" class="pokemonImage">
         <p class="stats">stats</p>
     </div>
     `;
     document.getElementById(`pokemonName${i}`).innerHTML = currentPokemon['name'];
+    document.getElementById(`pokemonId${i}`).innerHTML = '#' +currentPokemon['id'];
     document.getElementById(`pokemonImage${i}`).src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
 }
 
 
 function loadMorePokemon() {
     currentPokemonIndex += loadStop;
-    loadPokemon3();
+    loadPokemon();
 }
