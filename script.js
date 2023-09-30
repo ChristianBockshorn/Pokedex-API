035d11442cd78c930f306ba095c4e228f98ea891
Christian Bockshorn <bockshorn@gmx.net>
2023-09-30 09:28:56 +0200

    pokedeCard open-close edit2

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ async function loadPokemon() {
     const endIndex = currentPokemonIndex + loadStop;
 
     for (let i = startIndex; i <= endIndex; i++) {
-
         let url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
         let response = await fetch(url);
         let currentPokemon = await response.json();
@@ -17,58 +16,71 @@ async function loadPokemon() {
         allPokemon.push(currentPokemon);
 
         genPokeCard(i, currentPokemon);
+
     }
 }
-
-
 
 
 function genPokeCard(i, currentPokemon) {
     let pokeCard = document.getElementById(`card`);
     pokeCard.innerHTML += `
-    <div id="pokedex${i}" class="pokedex" onclick="openDialog()">
+    <div id="pokedex${i}" class="pokedex" onclick="openPokeCard(${i})">
         <div class="pokemonHeader">
             <h1 id="pokemonName${i}" class="pokemonName">Name</h1>    
             <h2 id="pokemonId${i}">#</h2>
-            
         </div>
         
         <img id="pokemonImage${i}" class="pokemonImage">
-        <p class="stats">stats</p>
-
         
-
-        
-    
     </div>
     `;
+    genPokeCardDetails(i, currentPokemon);
     document.getElementById(`pokemonName${i}`).innerHTML = currentPokemon['name'];
-    document.getElementById(`pokemonId${i}`).innerHTML = '#' +currentPokemon['id'];
+    document.getElementById(`pokemonId${i}`).innerHTML = '#' + currentPokemon['id'];
     document.getElementById(`pokemonImage${i}`).src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
-}
-
-function openDialog() {
-    document.getElementById(`dialog`).classList.remove('d-none');
-}
 
 
-function closeDialog() {
-    document.getElementById(`dialog`).classList.add('d-none');
 }
 
 
+function openPokeCard(i) {
+    document.getElementById(`pokeCard`).classList.remove('d-none');
+
+    genPokeCardDetails(i);
+
+}
+
+
+function closePokeCard() {
+    document.getElementById(`pokeCard`).classList.add('d-none');
+}
 
 
 function doNotClose(event) {
     event.stopPropagation();
 }
 
+
+function genPokeCardDetails(i, currentPokemon) {
+    document.getElementById(`pokemonName${i}`).innerHTML = currentPokemon['name'];
+    document.getElementById(`pokemonId${i}`).innerHTML = '#' + currentPokemon['id'];
+    document.getElementById(`pokemonImage${i}`).src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+
+    let pokeCardDetails = document.getElementById(`pokemonDetailInfo`);
+    pokeCardDetails.innerHTML += `
+    <div >
+    <h1 id="pokemonName${i}" class="pokemonName">Name</h1>    
+    <h2 id="pokemonId${i}">#</h2>
+    <img id="pokemonImage${i}" class="pokemonImage">     
+    </div>
+ `;
+
+    
+}
+
+
 function loadMorePokemon() {
     currentPokemonIndex += loadStop;
     loadPokemon();
 }
-
-
-
-
 
