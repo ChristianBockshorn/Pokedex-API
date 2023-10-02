805fe8a2da773bad4a5746512c80807e058405ed
Christian Bockshorn <bockshorn@gmx.net>
2023-10-02 17:03:20 +0200

    pokemon DetailCard edit

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ async function loadPokemon() {
 function genPokeCard(i, currentPokemon) {
     let pokeCard = document.getElementById(`card`);
     pokeCard.innerHTML += `
-    <div id="pokedex${i}" class="pokedex" onclick="openPokeCard(${i})">
+    <div id="pokedex${i}" class="pokedex" onclick="openPokeCard(${i - 1})">
         <div class="pokemonHeader">
             <h1 id="pokemonName${i}" class="pokemonName">Name</h1>    
             <h2 id="pokemonId${i}">#</h2>
@@ -34,7 +34,7 @@ function genPokeCard(i, currentPokemon) {
         
     </div>
     `;
-    genPokeCardDetails(i, currentPokemon);
+
     document.getElementById(`pokemonName${i}`).innerHTML = currentPokemon['name'];
     document.getElementById(`pokemonId${i}`).innerHTML = '#' + currentPokemon['id'];
     document.getElementById(`pokemonImage${i}`).src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
@@ -45,9 +45,7 @@ function genPokeCard(i, currentPokemon) {
 
 function openPokeCard(i) {
     document.getElementById(`pokeCard`).classList.remove('d-none');
-
     genPokeCardDetails(i);
-
 }
 
 
@@ -61,22 +59,77 @@ function doNotClose(event) {
 }
 
 
-function genPokeCardDetails(i, currentPokemon) {
-    document.getElementById(`pokemonName${i}`).innerHTML = currentPokemon['name'];
-    document.getElementById(`pokemonId${i}`).innerHTML = '#' + currentPokemon['id'];
-    document.getElementById(`pokemonImage${i}`).src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+function genPokeCardDetails(i) {
+
+    let pokemonName = allPokemon[`${i}`]['name'];
+    let pokemonId = allPokemon[`${i}`]['id'];
+    let pokemonImg = allPokemon[`${i}`]['sprites']['other']['official-artwork']['front_default'];
 
     let pokeCardDetails = document.getElementById(`pokemonDetailInfo`);
-    pokeCardDetails.innerHTML += `
-    <div >
-    <h1 id="pokemonName${i}" class="pokemonName">Name</h1>    
-    <h2 id="pokemonId${i}">#</h2>
-    <img id="pokemonImage${i}" class="pokemonImage">     
+
+    pokeCardDetails.innerHTML = `
+    <div class="pokemonDetailInfoContainer">
+        <div class="pokemonDetailInfoHeader">
+            <h1 id="pokemonName${i}" class="pokemonName">Name</h1>    
+            <h2 id="pokemonId${i}">#</h2>
+        </div>
+
+        <div class="pokemonDetailInfoImg">
+            <img id="pokemonImage${i}" class=""> 
+        </div>    
+
+        <div class="pokemonDetailInfoOverview">
+            <div class="pokemonDetailInfoOverviewHeader">
+                
+            </div>
+
+            <div class="statsDiv" id="aboutTab">
+                
+
+                <div class="container">
+                    <div class="box">
+                        <input type="radio" class="tab-toggle" name="tab-toggle" id="tab1" checked>
+                        <input type="radio" class="tab-toggle" name="tab-toggle" id="tab2">
+                        <input type="radio" class="tab-toggle" name="tab-toggle" id="tab3">
+
+                        <ul class="tab-list">
+                            <li class="tab-item">
+                                <label class="tab-trigger" for="tab1">About</label>
+                            </li>
+                            <li class="tab-item">
+                                <label class="tab-trigger" for="tab2">Stats</label>
+                            </li>
+                            <li class="tab-item">
+                                <label class="tab-trigger" for="tab3">Moves</label>
+                            </li>
+                        </ul>
+                        
+                        <div class="tab-container">
+                            <div class="tab-content">
+                                <span>Height</span>
+                                <span>Weight</span>
+                                <span>Abilities</span>
+                            </div>
+
+                            <div class="tab-content">Stats</div>
+
+                            <div class="tab-content">Moves</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>  
+        
     </div>
  `;
 
-    
+    document.getElementById(`pokemonName${i}`).innerHTML = pokemonName;
+    document.getElementById(`pokemonId${i}`).innerHTML = '#' + pokemonId;
+    document.getElementById(`pokemonImage${i}`).src = pokemonImg;
+
 }
+
 
 
 function loadMorePokemon() {
